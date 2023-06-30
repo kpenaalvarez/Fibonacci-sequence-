b01f1f4cb0fbf3a30b64256549c37e710e14e072
kpenaalvarez <136401504+kpenaalvarez@users.noreply.github.com>
2023-06-29 18:33:07 -0700

    Add files via upload

---
+++ Fibonacci Sequence.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 17:41:31 2023
+
+@author: kpenaalvarez
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+n = 100
+a = 0
+b = 1
+s = 0
+fibseq = []
+for x in range(n):
+    fibseq.append(s)
+    print(s,end=" ")
+    s = a+b
+    b = a
+    a = s
+plt.xlim(0, 25)
+plt.ylim(0,300)
+plt.plot(fibseq, color='red')
+plt.xlabel('Index')
+plt.ylabel('Fibonacci Bumber')
+plt.title('Fibonacci Sequence')
+plt.show()
